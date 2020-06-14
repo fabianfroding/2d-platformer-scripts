7ba8156b52f5a2fd8bc8647d20e0767c4a5929c6
fabianfroding <fabian.froding@gmail.com>
2020-06-15 00:52:26 +0200

    initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float runSpeed = 3f;
+
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+    }
+}
