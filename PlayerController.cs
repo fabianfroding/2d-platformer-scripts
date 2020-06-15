c1fb91900b6a6f432a277d2e29120ff236ad1582
fabianfroding <fabian.froding@gmail.com>
2020-06-16 01:02:19 +0200

    implement basic anims and controller scripts

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,37 +3,75 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float runSpeed = 3f;
+    private float runSpeed = 4f;
+    [SerializeField]
+    private float jumpSpeed = 5f;
 
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    Transform groundCheckL;
+
+    [SerializeField]
+    Transform groundCheckR;
+
+    Animator animator;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
+    bool isGrounded;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
 
-    private void FixedUpdate()
-    {
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Seer_Run");
+            }
             spriteRenderer.flipX = false;
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Seer_Run");
+            }
             spriteRenderer.flipX = true;
         }
         else
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            if (isGrounded)
+            {
+                animator.Play("Seer_Idle");
+            }
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            animator.Play("Seer_Jump");
         }
     }
 }
